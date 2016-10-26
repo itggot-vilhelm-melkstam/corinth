8b419ed33b3d7b9a13dda3b7fb1ec6ccb39de62f
Vilhelm Melkstam <vilhelm.melkstam@itggot.se>
2016-10-26 18:57:54 +0200

    Added cursor

--- lib/corinth.rb
+++ lib/corinth.rb
@@ -1,8 +1,29 @@
 class Corinth < Gosu::Window
 
 	def initialize
-		super 768, 512, false
+		super 768, 512, false, 100/1000
 		self.caption = "CORINTH"
+
+		setup
+	end
+
+	def setup
+		@cursor = Cursor.new(:cursor_1)
+		# @start_menu = StartMenu.new
+	end
+
+	def button_down(id)
+		close if id == Gosu::KbEscape
+	end
+
+	def update
+
+		@cursor.x, @cursor.y = self.mouse_x, self.mouse_y
+	end
+
+	def draw
+		draw_quad(0,0,0xffff8888, 0,512,0xffff8888 ,768,512,0xffff8888 ,768,0,0xffff8888)
+		@cursor.draw
 	end
 
 end
---
+++ lib/cursor.rb
@@ -0,0 +1,32 @@
+class Cursor
+
+	attr_accessor :x, :y
+
+	def initialize(style)
+		@x, @y = 0, 0
+		@style = style
+		@size = 24
+		setup
+	end
+
+	def setup
+		@styles = {}
+		Dir.glob("./media/cursor/*.png").each do |style|
+			@styles[style.scan(/\/([^\.\/]*).png/)[0][0].to_sym] = Gosu::Image.new(style)
+		end
+	end
+
+	def update
+
+	end
+
+	def style=(style)
+		raise ArgumentError, "Not an available cursorstyle" unless @styles.include? style
+		@style = style
+	end
+
+	def draw
+		@styles[@style].draw(@x, @y, 0, (@size/512.0), (@size/512.0))
+	end
+
+end
